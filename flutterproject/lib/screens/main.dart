23be13350a47a862dd5f945970eac606ec9f7035
SinHaw <202849l@mymail.nyp.edu.sg>
2022-07-31 23:41:13 +0800

    1

---
+++ flutterproject/lib/screens/main.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterproject/screens/Login.dart';
+import 'package:flutterproject/screens/Registration.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: RegistrationScreen(),
+    );
+  }
+}
